4e323a2255ff1f0ded096e63f4175b019a0bf690
Gilmar Pereira da Silva Junior <juninhsilva@live.com>
2024-07-28 16:44:19 -0300

    Update repet_txt.py

--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,13 @@
-# Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+# Vamos solicitar como entrada uma string e um número e repetir essa string tantas vezes quanto indicado pelo número.
+
+# Solicita ao usuário que insira uma string
+texto = input("Digite uma string: ")
+
+# Solicita ao usuário que insira um número
+numero = int(input("Digite um número: "))
+
+# Repite a string o número de vezes indicado
+resultado = texto * numero
+
+# Exibe o resultado
+print("Resultado:", resultado)
